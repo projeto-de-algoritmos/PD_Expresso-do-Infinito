d852cc48f295b9f3731c9986f1537c7cb82211b1
MatheusHenrick <matheu81h@gmail.com>
2023-11-25 18:07:42 -0300

    Início do projeto

---
+++ app/src/main/java/projeto/controle/expressodoinfinito/MainActivity.kt
@@ -0,0 +1,46 @@
+package projeto.controle.expressodoinfinito
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import projeto.controle.expressodoinfinito.ui.theme.ExpressoDoInfinitoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExpressoDoInfinitoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExpressoDoInfinitoTheme {
+        Greeting("Android")
+    }
+}
